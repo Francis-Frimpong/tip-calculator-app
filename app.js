79fa87e60c4897a5c993194f92e0d8fb09f7ee00
Francis <brakay1136@gmail.com>
2025-05-15 17:47:13 -0700

    built the input field focus functionality

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const inputBtn = document.querySelectorAll('.btns')
 
 const btnFormsCon = document.querySelector('.inputBtns-forms')
 
+const customField = document.querySelectorAll('input');
+
+
+
+
+customField.forEach((field) => {
+    field.addEventListener('focus', () => {
+        field.classList.add('focusedInput');
+    })
+
+    field.addEventListener('blur',() => {
+        field.classList.remove('focusedInput');
+
+    })
+})
+
 
 
 const tipPecentage = [5,10,15,25,50]
