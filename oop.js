fba122ffa55bba825a1bde19d57de7afd7894fc4
Francis <brakay1136@gmail.com>
2025-10-01 09:19:57 -0700

    add methods to the class

--- oop.js
+++ oop.js
@@ -1,6 +1,6 @@
 class TipCalculator {
   constructor() {
-    this.billAount = 0;
+    this.billAmount = 0;
     this.tipPercent = 0;
     this.numberOfPeople = 1;
 
@@ -10,12 +10,61 @@ class TipCalculator {
     this.peopleInput = document.querySelector(".people");
     this.errorMessage = document.querySelector(".error-msg");
     this.tipAmountDisplay = document.querySelector(".tip-amt");
-    this.tipAmountDisplay = document.querySelector(".total-amt");
+    this.totalAmountDisplay = document.querySelector(".total-amt");
     this.resetButton = document.querySelector(".reset-btn");
 
     this.btnFormsCon = document.querySelector(".inputBtns-forms");
     this.customField = document.querySelectorAll("input");
+
+    this.tipPercentage = [5, 10, 15, 25, 50];
   }
 
-  addEventListeners() {}
+  // bill input method
+  billinput() {
+    this.billAmount = this.billInput;
+    this.calculateTip();
+  }
+
+  buttonLinkToTipPercentageArr(e) {
+    if (e.target.classList.contains("btns")) {
+      this.tipButtons.forEach((button, index) => {
+        if (e.target === button) {
+          this.tipPercent = this.tipPercentage[index];
+          button.classList.add("activeButton");
+        }
+        button.classList.add("disabled");
+      });
+    }
+    this.calculateTip();
+  }
+
+  customtipInput() {
+    this.tipButtons.forEach((button) => {
+      button.classList.add("disabled");
+    });
+    this.tipPercent = this.customTipInput.value;
+    this.calculateTip();
+  }
+
+  calculateTip() {
+    if (this.billAmount > 0 && this.tipPercent > 0 && this.numberOfPeople > 0) {
+      let tipAmount =
+        (this.billAmount * this.tipPercent) / 100 / this.numberOfPeople;
+      let totalAmount = this.billAmount / this.numberOfPeople + tipAmount;
+
+      this.tipAmountDisplay.textContent = `$${tipAmount.toFixed(2)}`;
+      this.totalAmountDisplay.textContent = `$${totalAmount.toFixed(2)}`;
+    } else {
+      this.tipAmountDisplay.textContent = "$0.00";
+      this.totalAmountDisplay.textContent = "$0.00";
+    }
+  }
+
+  addEventListeners() {
+    this.billInput.addEventListener("input", () => this.billinput());
+    this.btnFormsCon.addEventListener("click", (e) =>
+      this.buttonLinkToTipPercentageArr(e)
+    );
+    this.customTipInput.addEventListener("input", () => this.customtipInput());
+  }
 }
